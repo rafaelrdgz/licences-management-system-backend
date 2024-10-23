bc6d8dd7e19bd5f0a3f6e172483e87383695c723
rafaelrdgz <rafaelrdgz2003@gmail.com>
2024-10-23 12:55:17 -0400

    added worker id validation

--- src/routes/workersRoutes.js
+++ src/routes/workersRoutes.js
@@ -1,4 +1,4 @@
-import {Router} from "express";
+import { Router } from "express";
 import pool from "../DB_config.js";
 import { verifyToken } from "../jwt-middleware.js";
 
@@ -7,8 +7,7 @@ router.use(verifyToken);
 // CREATE
 router.post("/workers", async (req, res) => {
   try {
-    const {id, name, email, lastNames, password, role} =
-      req.body;
+    const { id, name, email, lastNames, password, role } = req.body;
     console.log(name, email, lastNames, password, role);
     const result = await pool.query(
       "SELECT * FROM insert_trabajador($1, $2, $3, $4, $5, $6)",
@@ -16,7 +15,7 @@ router.post("/workers", async (req, res) => {
     );
     res.status(201).json(result.rows[0]);
   } catch (err) {
-    res.status(400).json({error: err.message});
+    res.status(400).json({ error: err.message });
   }
 });
 
@@ -26,7 +25,7 @@ router.get("/workers", async (req, res) => {
     const result = await pool.query("SELECT * FROM get_trabajadores()");
     res.status(200).json(result.rows);
   } catch (err) {
-    res.status(400).json({error: err.message});
+    res.status(400).json({ error: err.message });
   }
 });
 
@@ -39,17 +38,17 @@ router.get("/workers/:id", async (req, res) => {
     if (result.rows.length > 0) {
       res.status(200).json(result.rows[0]);
     } else {
-      res.status(404).json({error: "Worker not found"});
+      res.status(404).json({ error: "Worker not found" });
     }
   } catch (err) {
-    res.status(400).json({error: err.message});
+    res.status(400).json({ error: err.message });
   }
 });
 
 // UPDATE
 router.put("/workers/:id", async (req, res) => {
   try {
-    const {id, name, lastNames, email, password, role} = req.body;
+    const { id, name, lastNames, email, password, role } = req.body;
     console.log(id, name, lastNames, email, password, role);
     const result = await pool.query(
       "SELECT * FROM update_trabajador($1, $2, $3, $4, $5, $6)",
@@ -58,29 +57,48 @@ router.put("/workers/:id", async (req, res) => {
     console.log(result);
     res.status(200).json(result.rows[0]);
   } catch (err) {
-    res.status(400).json({error: err.message});
+    res.status(400).json({ error: err.message });
   }
 });
 
 // DELETE
 router.delete("/workers/:id", async (req, res) => {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
 
     if (!id) {
-      return res.status(400).json({error: "ID is required"});
+      return res.status(400).json({ error: "ID is required" });
     }
 
     const result = await pool.query("SELECT delete_trabajador($1)", [id]);
 
     if (result.rows.length === 0) {
-      return res.status(404).json({error: "Worker not found"});
+      return res.status(404).json({ error: "Worker not found" });
     }
-    console.log(result)
+    console.log(result);
     res.status(204).send(); // Respuesta exitosa sin contenido
   } catch (err) {
     console.error("Error deleting worker:", err);
-    res.status(400).json({error: err.message});
+    res.status(400).json({ error: err.message });
+  }
+});
+
+router.get("/workers/exists/:id/", async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(id);
+    const result = await pool.query("SELECT * FROM get_trabajador_by_id($1)", [
+      id,
+    ]);
+
+    if (result.rowCount > 0) {
+      res.status(200).json({ exists: true });
+    } else {
+      res.status(404).json({ exists: false });
+    }
+  } catch (err) {
+    console.error("Error checking worker existence:", err);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
